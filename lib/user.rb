535fe549c87a09557a96f95b989715952e990ac6
tugi <blueqwinn@gmail.com>
2023-02-13 22:23:24 +0300

    fifth commit

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User 
+  def initialize user.do |user|
+    @user = User.new
+    user.new = "has a first name"
+    user.first_name= "Tracy"
+    user.first_name =("Morgan")
+  end
+
+ def initialize last_name.do |last_name|
+@last_name = last_name
+last_name.new ="has a last name"
+user.last_name =("Morgan")
+ end
+end
