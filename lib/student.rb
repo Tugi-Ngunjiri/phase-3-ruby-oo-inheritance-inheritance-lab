523f6f955fe9181e58af5c7b80f358fd62f57799
tugi <blueqwinn@gmail.com>
2023-02-13 21:50:48 +0300

    first commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,39 @@
 class Student 
 
+    def student. do |student|
+ @student = student
+ student.new = "Inherits from the User Class"
+student.superclass= :User
+    end
 end
+
+def first_name. do  |first_name|
+   @first_name= first_name
+   first_name= "has a first name"
+  
+   student.first_name ="Steve"
+student.first_name "Jobs"
+ end
+end
+
+def  initialize .new . do |new|
+ Knowledge Array[]
+
+@Sophie = Student.new
+sophie.instance_variable_get(Knowledge)=[()]
+ end
+end
+
+def learn .do  |learn|
+    @learn= learn
+    learn.new ="takes in an argument of a string of knowledge and adds it to the student's knowledge array"
+   Student.learn="Ruby framework Rails gem bundle update"
+Student.instance_variable_get((knowledge)).to include("Ruby framework Rails gem bundle update")
+end
+end
+
+def knowledge.do |knowledge|
+    @knowledge = knowledge.
+    knowledge.new ="returns that student's knowledge array"
+student.learn ="Javascript Ember Elixir knowledge"
+student.knowledge.to include("Javascript Ember Elixir knowledge")
